79e7dbb9d9d5a49085a196f849faac8a9d899bd3
KotaroKazama0624 <ec27096h@gmail.com>
2023-01-21 23:49:49 +0900

    javascriptの基本文法をマスター

---
+++ index.js
@@ -0,0 +1,47 @@
+let unko = 'Hello World!';
+
+// unko = 'Hello World2!';
+
+// 定数
+const bigUnko = 'bigUnko';
+// bigUnko = 'H'
+
+// 配列
+let inoki = ['1','2','3'];
+
+// ループ分
+// let index = 0;
+// while(index < inoki.length){
+//   console.log(inoki[index])
+//   index++;
+// }
+
+// if(inoki.length > 3){
+//   console.log("ボンバイエ!");
+// } else {
+//   console.log('ボンバ...!');
+// }
+
+// 関数
+// const test = (arg) => {
+//   // ここに実行したい命令を書く
+//   if(inoki.length > arg){
+//     console.log("ボンバイエ!");
+//   } else {
+//     console.log('ボンバ...!');
+//   }
+// };
+
+// test(2);
+
+// オブジェクト
+const unko2 = {
+  color: 'pink',
+  size: 'large',
+  purfume: 'mint',
+  goToilet: () => {
+    console.log('Hello world!');
+  }
+};
+
+console.log(unko2.goToilet());
